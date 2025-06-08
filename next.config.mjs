e87c05fe3cf28d6d4f33892ae2bb492eb0fe4e3a
CodemeBilguun <codeme.bilguun@gmail.com>
2025-06-09 01:19:35 +0800

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -17,54 +17,12 @@ const nextConfig = {
   },
   experimental: {
     serverActions: true,
-  
     skipTrailingSlashRedirect: true,
-   
     serverComponentsExternalPackages: [],
-   
     allowedRevalidateHeaderKeys: [],
-    
-    webVitalsAttribution: ['all']
+    webVitalsAttribution: ["all"],
   },
- 
   onDemandEntries: {
-
-    maxInactiveAge: 25 * 1000,
-    pagesBufferLength: 4,
-  },
-};
-
-export default nextConfig;/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "fakestoreapi.com",
-      },
-    ],
-    unoptimized: true,
-  },
-  experimental: {
-    serverActions: true,
-    
-    skipTrailingSlashRedirect: true,
-   
-    serverComponentsExternalPackages: [],
-    
-    allowedRevalidateHeaderKeys: [],
-    
-    webVitalsAttribution: ['all']
-  },
- 
-  onDemandEntries: {
-    
     maxInactiveAge: 25 * 1000,
     pagesBufferLength: 4,
   },
